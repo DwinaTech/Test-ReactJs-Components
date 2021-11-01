4d5ff9736c0af70d3f073e19658d8d8d24725573
Mohammed Dwina <diwina1993@hotmail.com>
2021-11-01 12:11:33 +0000

    Create JSON server

--- src/containers/dashbaord/hooks.js
+++ src/containers/dashbaord/hooks.js
@@ -1,8 +1,8 @@
 import React from "react";
+import axios from "axios";
 
 export const useDashboard = () => {
-  const date = new Date();
-  const userId = date.toLocaleDateString();
+  const userId = new Date().getTime;
 
   const [users, setUsers] = React.useState([]);
 
@@ -13,6 +13,27 @@ export const useDashboard = () => {
     id: "",
   });
 
+  const fetchUsers = async () => {
+    try {
+      const { data } = await axios.get(`http://localhost:5000/users`);
+      setUsers(data);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  const addUser = async () => {
+    try {
+      await axios.post(`http://localhost:5000/users`, {
+        ...user,
+        id: userId,
+      });
+      fetchUsers();
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
   const handleChange = (e) => {
     const { name, value } = e.target;
     setUser({ ...user, [name]: value });
@@ -20,8 +41,8 @@ export const useDashboard = () => {
 
   const handleSubmit = (e) => {
     e.preventDefault();
+    addUser();
     setUser({ firstName: "", lastName: "", email: "", id: "" });
-    setUsers([...users, { ...user, id: userId }]);
   };
 
   return { user, users, handleChange, handleSubmit };
